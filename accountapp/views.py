e8983a741ec7d634fa133d8bc8e9d51f5222ca3f
zlfcksdid <zlfcksdid@naver.com>
2021-07-16 10:17:55 +0900

    7/16 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -33,4 +33,5 @@ class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
-    template_name ='accountapp/create.hmtl'
+    template_name ='accountapp/create.html'
+
